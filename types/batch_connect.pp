d0534ed08e65bfced60ee0f7f1cbeff3c8c73ac2
Matt Mix <mattmix@bluemavid.com>
2022-06-10 14:06:58 -0500

    Add the ability to configure set_host for clusters (#98)
    * Allow set_host under batch_connect type
    
    * Add set_host to template
    
    Co-authored-by: Matt Mix <mattmix@umn.edu>
    Co-authored-by: treydock <treydock@gmail.com>

--- types/batch_connect.pp
+++ types/batch_connect.pp
@@ -1,6 +1,12 @@
 # Defines cluster config batch_connect values
 type Openondemand::Batch_connect = Struct[{
-  Optional['basic']     => Struct[{'script_wrapper' => String}],
-  Optional['vnc']       => Struct[{'script_wrapper' => String}],
+  Optional['basic']     => Struct[{
+        Optional['script_wrapper'] => String,
+        Optional['set_host'] => String
+    }],
+  Optional['vnc']       => Struct[{
+        Optional['script_wrapper'] => String,
+        Optional['set_host'] => String
+    }],
   Optional['ssh_allow'] => Boolean,
 }]
--- types/dashboard_layout.pp
+++ types/dashboard_layout.pp
@@ -2,4 +2,4 @@
 type Openondemand::Dashboard_layout = Struct[{
   'rows' => Array[Struct[{'columns' => Array[Openondemand::Dashboard_layout_column]}]]
 }]
-  
+
