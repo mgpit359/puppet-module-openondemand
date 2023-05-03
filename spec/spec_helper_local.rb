732cbab4e68843e65e641e017d3bd2884d43a127
treydock <tdockendorf@osc.edu>
2023-05-03 14:02:49 -0400

    Support Puppet 8, Drop Puppet 6 support, update dependencies (#125)
    * Support puppetlabs/apt 9.x
    * Support puppetlabs/apache 10.x
    * Support puppetlabs/vcsrepo 6.x
    * Support puppet/augeasproviders_shellvar 5.x
    * Support puppet/systemd 4.x

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -2,15 +2,3 @@
 
 dir = __dir__
 Dir["#{dir}/shared_examples/**/*.rb"].sort.each { |f| require f }
-
-require 'rspec-puppet-facts'
-include RspecPuppetFacts # rubocop:disable Style/MixinUsage
-
-add_custom_fact :service_provider, ->(_os, facts) {
-  case facts[:operatingsystemmajrelease]
-  when '6'
-    'redhat'
-  else
-    'systemd'
-  end
-}
